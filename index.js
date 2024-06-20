d1308030710bcde80264525faf373ccee5b68d85
PrashantPatil31 <prashantfp3@gmail.com>
2024-06-20 11:15:38 +0530

    userdata

--- index.js
+++ index.js
@@ -3,13 +3,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const userData = {
+    "firstName": "John",
+    "lastName": "Doe",
+    "email": "john.doe@example.com",
+    "userId": "123456",  // Replace with a unique identifier for the user
+    "isActive": true,
+    "preferences": {
+      "language": "en",
+      "theme": "light"
+    },
+    "addresses": [
+      {
+        "type": "home",
+        "streetAddress": "123 Main Street",
+        "city": "Anytown",
+        "state": "CA",
+        "postalCode": "12345"
+      }
+    ]
+  }
+  
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/login', (req ,res)=>{
 res.send("login successfully")
 })
-
+app.get('/userdata' ,(req,res) =>{
+    res.json(userData)
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
